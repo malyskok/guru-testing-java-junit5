049aa1d8f2bbd68eac318077c825f3b75c7475b6
malyskok <kamil.malysko@gmail.com>
2023-02-05 10:26:18 +0100

    JUnit disabling tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("Disable until we learn to mock")
+class OwnerSDJpaServiceTest {
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Disabled
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
