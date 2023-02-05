8463b824cd317d32fa98e065a228a2870c7a7f80
malyskok <kamil.malysko@gmail.com>
2023-02-05 10:27:32 +0100

    JUnit DisplayName

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +16,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Tests if index returns correct view")
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "wrong index returned");
