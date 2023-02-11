967c0822d0c9bd16fa6ed21ade82a8073b623b20
malyskok <kamil.malysko@gmail.com>
2023-02-11 16:20:35 +0100

    Adding VetControllerTest

--- src/main/java/guru/springframework/sfgpetclinic/fauxspring/Model.java
+++ src/main/java/guru/springframework/sfgpetclinic/fauxspring/Model.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.fauxspring;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Map;
+import java.util.Set;
+
 public interface Model {
 
     void addAttribute(String key, Object o);
 
     void addAttribute(Object o);
+
+    Map<String, Object> getMap();
 }
---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.fauxspring.ModelMapImpl;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
+import guru.springframework.sfgpetclinic.services.map.VetMapService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetControllerTest {
+
+    private VetController vetController;
+    private VetService vetService;
+    private SpecialtyService specialtyService;
+
+    @BeforeEach
+    void setUp() {
+        specialtyService = new SpecialityMapService();
+        vetService = new VetMapService(specialtyService);
+        vetController = new VetController(vetService);
+
+        Vet vet1 = new Vet(1L, "John", "Doe", null);
+        Vet vet2 = new Vet(2L, "Jimmy", "Smith", null);
+        vetService.save(vet1);
+        vetService.save(vet2);
+    }
+
+    @Test
+    void listVets() {
+        // Given
+        Model model = new ModelMapImpl();
+
+        // When
+        String view = vetController.listVets(model);
+
+        // Then
+        assertEquals("vets/index", view);
+        Set vets = (Set) model.getMap().get("vets");
+        assertEquals(2, vets.size());
+    }
+}
