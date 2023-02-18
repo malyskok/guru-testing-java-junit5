c2c08e85dcfec6dbe27aeb5a54cda4e929bb26c5
malyskok <kamil.malysko@gmail.com>
2023-02-18 10:31:14 +0100

    junit default methods

--- src/test/java/guru/springframework/sfgpetclinic/ControllersTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllersTest.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllersTest {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll for controller tag");
+    }
 }
